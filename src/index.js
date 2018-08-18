869ccc92bbe25cea0c21af3771bd3f8f8eadab63
Khiem <khiemduy90@gmail.com>
2018-08-18 11:17:28 -0500

    Created card list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import { robots } from './robots';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
 ReactDOM.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name}
-        email={robots[0].email} />
-        <Card id={robots[2].id} name={robots[2].name}
-        email={robots[2].email} />
-        <Card id={robots[4].id} name={robots[4].name}
-        email={robots[4].email} /> 
-    </div>,
-    document.getElementById('root'));
+   <CardList robots={robots}/>,
+   document.getElementById('root'));
 registerServiceWorker();
