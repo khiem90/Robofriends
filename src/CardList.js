4c76bc3f2eb6375d1d5cf2891f44575697794c68
Khiem <khiemduy90@gmail.com>
2018-08-19 15:16:27 -0500

    Added search box and filter feature

--- src/CardList.js
+++ src/CardList.js
@@ -2,15 +2,25 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const cardComponent = robots.map((user, i) => {
-        return  <Card id={robots[i].id} name={robots[i].name}
-        email={robots[i].email} />
-    })
     return (
         <div>
-           {cardComponent} 
+            {
+               robots.map((user, i) => {
+                   return (
+                        <Card
+                            key={i}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                            id={robots[i].id}
+                        />
+                   )
+                })
+            }
         </div>
     );
 }
+
+
+
 
 export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
-import { robots } from './robots';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(
-   <CardList robots={robots}/>,
-   document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('root'));
 registerServiceWorker();
