5837ab76881515566a45c2b934a97c08a41b232e
Khiem90 <khiemduy90@gmail.com>
2018-09-27 23:58:31 -0500

    Added Redux; created actions, reducers and store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 
@@ -20,15 +34,12 @@ class App extends Component {
         .then(users => this.setState({ robots: users }));
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
     render() {
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
             return (
-                robot.name.toLowerCase().includes(searchfield.toLowerCase())
+                robot.name.toLowerCase().includes(searchField.toLowerCase())
             )
          });
          
@@ -39,9 +50,11 @@ class App extends Component {
                 <Fragment>
                     <div className='tc'>
                         <h1 className='f1'>Robofriends</h1>
-                        <SearchBox searchChange={this.onSearchChange}/>
+                        <SearchBox searchChange={onSearchChange}/>
                         <Scroll>
-                            <CardList robots={filteredRobots} />
+                            <ErrorBoundary>
+                                <CardList robots={filteredRobots} />
+                            </ErrorBoundary>
                         </Scroll>
                     </div>
                 </Fragment>
@@ -51,4 +64,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
